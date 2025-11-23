7b045d33f313d41a8a7a21be0666e84bd2df427a
Vikram Ramavarapu <vikramr2@illinois.edu>
2025-11-23 16:47:53 +0000

    fixed hcat model

--- hcat/trainer.py
+++ hcat/trainer.py
@@ -41,6 +41,7 @@ def train_epoch(
         # Backward pass
         optimizer.zero_grad()
         loss.backward()
+        torch.nn.utils.clip_grad_norm_(model.parameters(), max_norm=1.0)
         optimizer.step()
 
         total_loss += loss.item()
